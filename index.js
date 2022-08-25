0420407cb37f7472cd4684846e12096d78732923
ismam3 <ismamlabib3@gmail.com>
2022-08-25 13:48:36 -0700

    PORT; typo is solved

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get("/about",(req, res)=>{
     res.render('about');
 })
 
-app.listen(process.env.port || 3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("Server is running");
 })
